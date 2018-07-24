c00dd68a735a0ae6653c7d645b64f46f36a755ae
cain910772 <cain910772@yahoo.com>
2018-07-23 20:14:42 -0500

    first

---
+++ main.js
@@ -0,0 +1,11 @@
+// challenge1
+var button = document.querySelector("#myButton");
+button.addEventListener("click", function() {
+  alert("Hellllooo, it seems to work!!");
+});
+function myFunction() {
+    var sendTo = document.getElementsById("article")[0];
+    var att = document.createAttribute("congressional-district");
+    att.value = "Tennesee's 5th";
+    h1.setAttributeNode(att);
+}
